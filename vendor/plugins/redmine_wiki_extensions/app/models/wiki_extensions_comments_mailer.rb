9ca4029a919f075875a9a45ea9b555184d1506c1
Corvus <christian.haeusler@piratenpartei.ch>
2012-05-06 12:04:53 +0200

    Wiki extensions
    * Added version 0.3.9 of the wiki extension plugin
    * Added version 0.1.0 of the local avatar plugin
    * Added wiki notes as submodule

---
+++ vendor/plugins/redmine_wiki_extensions/app/models/wiki_extensions_comments_mailer.rb
@@ -0,0 +1,24 @@
+require 'mailer'
+
+class WikiExtensionsCommentsMailer < Mailer
+  def wiki_commented(comment, wiki_page)
+    project = wiki_page.project
+    author = comment.user
+    text = comment.comment
+    redmine_headers 'Project' => project,
+                    'Wiki-Page-Id' => wiki_page.id,
+                    'Author' => author
+    message_id wiki_page
+    # Send notification to watchers of wiki page
+    recipients wiki_page.watchers.collect{|watcher| watcher.user.mail}
+    subject "[#{project.name} - Wiki - #{wiki_page.title}] commented"
+    body = {
+      :project => project,
+      :author => author,
+      :text => text,
+      :wiki_page_title => wiki_page.title,
+      :wiki_page_url => url_for(:controller => 'wiki', :action => 'show', :project_id => project, :id => wiki_page.title)
+    }
+    render_multipart('wiki_commented', body)
+  end
+end
